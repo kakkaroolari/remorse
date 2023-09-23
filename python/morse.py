9fcc0cf242f751783d262f7552086de910239559
kakkaroolari <homorunkkari2@hotmail.com>
2023-09-23 12:28:08 +0300

    parannuksia

--- python/morse.py
+++ python/morse.py
@@ -49,17 +49,48 @@ taulukko = {
 # 5. The space between words is 7 time units.
 timefactor = 0.05 # 50 millisecs
 
+# from enum import Enum
+# class MorseAction(Enum):
+#     LETTER = 1
+#     WORDSPACE = 2
+def tarkista( syote):
+    kaikki_ok = True
+    for kirjain in syote:
+        if kirjain not in taulukko and kirjain is not ' ':
+            kaikki_ok = False
+            break # ensimmainen outo merkki, lopeta etsinta
+    if not kaikki_ok:
+        print("viesti sisaltaa ei morse-merkkeja")
+
+def printAsIs(morsemerkki, endofline):
+    if morsemerkki is None:
+        print("   ", end="") # sanaväli
+    else:
+        print(morsemerkki, end=" ")
+    if endofline:
+        print() # rivinvaihto
+    sleep(timefactor)
+
 if __name__ == "__main__":
     # input
     print("syota lause (ja paina enter):\n")
     input1 = input()
 
+    # tarkista teksti on koodattavissa
+    tarkista(input1)
+
     # juokse läpi
+    counter = 0
     for kirjain in input1:
-        if kirjain not in taulukko:
-            print()
+        counter = counter + 1
+        if kirjain is ' ':
+            morsemerkki = None
+        elif kirjain not in taulukko:
+            # skip
             continue
-        morsemerkki = taulukko[kirjain]
-        print(morsemerkki, " ")
-        sleep(timefactor)
+        else:
+            morsemerkki = taulukko[kirjain]
+        endofline = counter == len(input1)
+        printAsIs(morsemerkki, endofline)
+    
 
